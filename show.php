3a2da17b3bcbe97a02c0be6066966fa9bc5f05e0
Dmondial <www.ezweb.1ocofrank.acidman@gmail.com>
2021-01-22 23:11:47 +0900

    Initial commit

---
+++ show.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>uranai</title>
+  </head>
+  <body>
+    <h1>ギターです！</h1>
+    
+  </body>
+</html>
